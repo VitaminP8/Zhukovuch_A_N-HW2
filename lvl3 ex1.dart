a8bee1ba14a9e738cb3a4561616f29b407822d16
Alexey <alexey.zhukovich@gmail.com>
2021-10-11 15:16:25 +0300

    lvl3 ex1

---
+++ lvl3 ex1.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+  print (fibonacci(n));
+}
+
+int fibonacci (int n){
+  if (n == 1){
+    return 0;
+  }
+  else if (n == 2){
+    return 1;
+  }
+  else {
+    return (fibonacci(n-1) + fibonacci(n-2));
+  }
+}
